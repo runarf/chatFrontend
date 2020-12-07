d2f968c72f1260ca9b8410dbb3f0bfb4b8903ad0
Runar <runar.flaten@zalando.de>
2020-12-07 19:08:52 +0100

    create chat skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+interface MessageContainer {
+  id: number;
+  date: string;
+  message: string;
+  author: string;
+}
+
 function App() {
+  const [name, setName] = useState('');
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState<MessageContainer[]>([]);
   return (
     <div className="App">
+      <div>
+        Your name
+        <input type="text" onChange={(e) => setName(e.target.value)} value={name} />
+      </div>
+      <div id="chatWindow">
+        <div id="messages">
+          {messages.map((messageContainer) => {
+            const { id, author, message, date } = messageContainer;
+            return (
+              <div key={id}>
+                <div>{author}</div>
+                <div>{message}</div>
+                <div>{date}</div>
+              </div>
+            );
+          })}
+        </div>
+        <input type="text" onChange={(e) => setMessage(e.target.value)} value={message} />
+        <button
+          onClick={() => {
+            setMessage('');
+          }}
+        >
+          Send
+        </button>
+      </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
